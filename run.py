33766a3b23c1123f214a14b2297476aecf8af95f
declanoller <declanoller@gmail.com>
2019-11-01 00:15:06 -0400

    made working back and forth simple example

--- run.py
+++ run.py
@@ -1,1 +1,18 @@
-print("Hello RaaS World")
+import gym
+import gym_raas
+import numpy as np
+import time
+
+
+env = gym.make('raaspendulum-v0')
+env.reset()
+obs = []
+for i in range(50):
+    observation, reward, done, info = env.step([1])
+    time.sleep(0.01)
+    obs.append(observation)
+
+for i in range(50):
+    observation, reward, done, info = env.step([-1])
+    time.sleep(0.01)
+    obs.append(observation)
