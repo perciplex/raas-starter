124264a10b72d18890984ff79bbee60621eab196
Maximilian King <mxln.king@gmail.com>
2019-11-01 03:01:57 -0400

    Stdout for container
    Print statements so we can see progress from docker

--- run.py
+++ run.py
@@ -8,11 +8,15 @@ env = gym.make('raaspendulum-v0')
 env.reset()
 obs = []
 for i in range(50):
+    print("Step {}: moving forward".format(i))
     observation, reward, done, info = env.step([1])
+    print("\tObs: {}\tRew: {}".format(observation, reward))
     time.sleep(0.01)
     obs.append(observation)
 
 for i in range(50):
+    print("Step {}: moving backwards".format(i))
     observation, reward, done, info = env.step([-1])
+    print("\tObs: {}\tRew: {}".format(observation, reward))
     time.sleep(0.01)
     obs.append(observation)
